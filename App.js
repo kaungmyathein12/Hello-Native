af3804de585a5b0abe483a285b08ae87fe18ad93
kaungmyathein12 <kaungmyathein.dev@gmail.com>
2023-01-25 11:48:13 +0630

    add profile section

--- App.js
+++ App.js
@@ -8,7 +8,12 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          animation: "slide_from_right",
+          presentation: "card",
+        }}
+      >
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Profile" component={ProfileScreen} />
       </Stack.Navigator>
